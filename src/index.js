9a4116d4e370eb2344359fcdead57a8337bea32e
Andrey Krivosheev <feuiuxbe@gmail.com>
2021-03-13 17:05:25 +0100

    feat: create solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
 exports.min = function min (array) {
-  return 0;
+  if ((typeof (array) === 'undefined') || (array.length === 0)) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((typeof (array) === 'undefined') || (array.length === 0)) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((typeof (array) === 'undefined') || (array.length === 0)) return 0;
+  let avgnum;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += parseInt(array[i], 10); 
+  }
+  return (avgnum = sum/array.length);
 }
